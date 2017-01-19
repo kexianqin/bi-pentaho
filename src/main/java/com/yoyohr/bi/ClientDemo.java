bc3a173f34338f14234bae307142842b23a58a54
lionjiang <jlm0808@126.com>
2017-01-19 13:11:03 +0800

    add ClientDemo.java

---
+++ src/main/java/com/yoyohr/bi/ClientDemo.java
@@ -0,0 +1,18 @@
+package com.yoyohr.bi;
+
+/**
+ * HttpClient使用案例
+ *
+ * @author  Leo <jiangwenhua@yoyohr.com>
+ */
+public class ClientDemo {
+
+    public String get() {
+        //TODO 实现Get方法
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+}
